d03255df7cb389ee0e3605af028f1c127354a080
Wojciech Migda <wojtek.golf@interia.pl>
2015-02-21 19:43:35 +0100

    input data preparation

---
+++ src/ChildStuntedness5.hpp
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
+ *
+ * Filename: ChildStuntedness5.hpp
+ *
+ * Description:
+ *      description
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2015-02-21   wm              Initial version
+ *
+ ******************************************************************************/
+
+#ifndef CHILDSTUNTEDNESS5_HPP_
+#define CHILDSTUNTEDNESS5_HPP_
+
+#include <vector>
+#include <string>
+
+typedef double real_type;
+
+struct ChildStuntedness5
+{
+    std::vector<double>
+    predict(
+        int testType,
+        int scenario,
+        std::vector<std::string> & training,
+        std::vector<std::string> & testing) const;
+};
+
+std::vector<double>
+ChildStuntedness5::predict(
+    int testType,
+    int scenario,
+    std::vector<std::string> & training,
+    std::vector<std::string> & testing) const
+{
+    std::vector<double> result;
+
+    return result;
+}
+
+#endif /* CHILDSTUNTEDNESS5_HPP_ */
--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@
  ******************************************************************************/
 
 #include "ChildStuntedness5.hpp"
+#include "num.hpp"
 
 #include <fstream>
 #include <vector>
@@ -34,6 +35,51 @@
 #include <cassert>
 #include <valarray>
 #include <iterator>
+#include <utility>
+
+std::vector<std::string> read_file(std::string && fname)
+{
+    std::ifstream fcsv(fname);
+    std::vector<std::string> vcsv;
+
+    for (std::string line; std::getline(fcsv, line); /* nop */)
+    {
+        vcsv.push_back(line);
+    }
+    fcsv.close();
+
+    return vcsv;
+}
+
+std::vector<std::pair<num::size_type, num::size_type>>
+extract_subject_ranges(std::vector<std::string> && vstr)
+{
+    assert(vstr.size() > 1);
+
+    std::vector<std::pair<num::size_type, num::size_type>> result;
+
+    num::size_type first{0};
+    int curr_id = std::atoi(vstr[first].c_str());
+
+    for (num::size_type idx{1}; idx < vstr.size(); ++idx)
+    {
+        const int id = std::atoi(vstr[idx].c_str());
+
+        if (id != curr_id)
+        {
+            result.emplace_back(first, idx - 1);
+            first = idx;
+            curr_id = id;
+        }
+        else
+        {
+            ;
+        }
+    }
+    result.emplace_back(first, vstr.size() - 1);
+
+    return result;
+}
 
 int main(int argc, char **argv)
 {
@@ -42,130 +88,37 @@ int main(int argc, char **argv)
 
     std::cerr << "SEED: " << SEED << ", CSV: " << FNAME << std::endl;
 
-    std::ifstream fcsv(FNAME);
-    std::vector<std::string> vcsv;
-
-    for (std::string line; std::getline(fcsv, line);)
-    {
-        vcsv.push_back(line);
-    }
-    fcsv.close();
+    const std::vector<std::string> vcsv = read_file(std::string(FNAME));
 
     std::cerr << "Read " << vcsv.size() << " lines" << std::endl;
 
+    std::vector<std::pair<num::size_type, num::size_type>> subject_ranges =
+        extract_subject_ranges(std::vector<std::string>{vcsv});
+
+    std::mt19937 g(SEED);
+    std::shuffle(subject_ranges.begin(), subject_ranges.end(), g);
+
+    const std::size_t PIVOT = 0.67 * subject_ranges.size();
+
+    std::vector<std::string> train_data;
+    for (auto it = subject_ranges.cbegin(); it != subject_ranges.cbegin() + PIVOT; ++it)
     {
-        std::mt19937 g(SEED);
-        std::shuffle(vcsv.begin(), vcsv.end(), g);
+        train_data.insert(train_data.end(), vcsv.cbegin() + it->first, vcsv.cbegin() + it->second + 1);
     }
 
-    const std::size_t PIVOT = 0.67 * vcsv.size();
-
-    std::vector<std::string> train_data(vcsv.cbegin(), vcsv.cbegin() + PIVOT);
-    std::vector<std::string> test_data(vcsv.cbegin() + PIVOT, vcsv.cend());
-
-    const std::size_t N = std::count_if(test_data.cbegin(), test_data.cend(),
-        [](const std::string & line) -> bool
-        {
-            const std::size_t pos = line.rfind(',');
-            assert(pos != std::string::npos);
-            const int last_val = std::atoi(line.c_str() + pos + 1);
-            return last_val > 0;
-        }
-    );
-    std::cerr << "N: " << N << std::endl;
-
-    const std::size_t M = std::count_if(test_data.cbegin(), test_data.cend(),
-        [](const std::string & line) -> bool
-        {
-            const std::size_t pos = line.rfind(',');
-            assert(pos != std::string::npos);
-            const int last_val = std::atoi(line.c_str() + pos + 1);
-            return last_val > 1;
-        }
-    );
-    std::cerr << "M: " << M << std::endl;
-
-    std::sort(test_data.begin(), test_data.end(),
-        [](const std::string & lhs, const std::string & rhs) -> bool
-        {
-            const std::size_t lpos = lhs.rfind(',');
-            const std::size_t rpos = rhs.rfind(',');
-            assert(lpos != std::string::npos);
-            assert(rpos != std::string::npos);
-
-            const int last_l_val = std::atoi(lhs.c_str() + lpos + 1);
-            const int last_r_val = std::atoi(rhs.c_str() + rpos + 1);
-
-            return last_l_val > last_r_val;
-        }
-    );
-
-    for (auto & item : test_data)
+    std::vector<std::string> test_data;
+    for (auto it = subject_ranges.cbegin() + PIVOT; it != subject_ranges.cend(); ++it)
     {
-        constexpr std::size_t TEST_NCOL{29};
-        std::size_t ncomma{0};
-
-        item.resize(std::distance(item.cbegin(), std::find_if(item.cbegin(), item.cend(),
-            [&ncomma](const char & ch)
-            {
-                if (ch == ',' && ncomma == (TEST_NCOL - 1))
-                {
-                    return true;
-                }
-                else
-                {
-                    ncomma += (ch == ',');
-                    return false;
-                }
-            }
-        )));
-        assert(std::count(item.cbegin(), item.cend(), ',') == (TEST_NCOL - 1));
+        test_data.insert(test_data.end(), vcsv.cbegin() + it->first, vcsv.cbegin() + it->second + 1);
     }
 
+    std::cerr << "Train data has " << train_data.size() << " elements" << std::endl;
+    std::cerr << "Test data has " << test_data.size() << " elements" << std::endl;
+
     ////////////////////////////////////////////////////////////////////////////
-    TripSafetyFactors worker;
-    std::vector<int> prediction = worker.predict(train_data, test_data);
+    const ChildStuntedness5 worker;
+    std::vector<double> prediction = worker.predict(0, 0, train_data, test_data);
     ////////////////////////////////////////////////////////////////////////////
-
-    std::valarray<float> scores(test_data.size());
-    std::size_t index = 1;
-    std::generate(std::begin(scores), std::end(scores),
-        [&N, &index]()
-        {
-            return std::max((2.0f * N - index++) / (2 * N), 0.0f);
-        }
-    );
-    std::valarray<float> bonuses(test_data.size());
-    index = 1;
-    std::generate(std::begin(bonuses), std::end(bonuses),
-        [&M, &index]()
-        {
-            return std::max(0.3f * (2.0f * M - index++) / (2 * M), 0.0f);
-        }
-    );
-
-    const float MAX_POINTS =
-        std::accumulate(std::begin(scores), std::begin(scores) + N, 0.0f) +
-        std::accumulate(std::begin(bonuses), std::begin(bonuses) + M, 0.0f);
-
-    const float POINTS =
-        std::accumulate(prediction.cbegin(), prediction.cbegin() + N, 0.0f,
-            [&scores](const float & a, const int & i)
-            {
-                return scores[i - 1] + a;
-            }
-        ) +
-        std::accumulate(prediction.cbegin(), prediction.cbegin() + M, 0.0f,
-            [&bonuses](const float & a, const int & i)
-            {
-                return bonuses[i - 1] + a;
-            }
-        );
-
-    std::cerr << "MAX_POINTS: " << MAX_POINTS << std::endl;
-    std::cerr << "POINTS: " << POINTS << std::endl;
-
-    std::cerr << "SCORE: " << (int)std::round(1000000 * POINTS / MAX_POINTS) << std::endl;
 
     return 0;
 }
