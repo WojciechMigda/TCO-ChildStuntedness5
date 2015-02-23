fcba82c1356d559d8e03c504145e6808244f95a5
Wojciech Migda <wojtek.golf@interia.pl>
2015-02-23 19:41:12 +0100

    make_submission.sh

--- src/make_submission.sh
+++ src/make_submission.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat num.hpp array2d.hpp extract_subject_ranges.hpp ChildStuntedness5.hpp | grep -v "#include \"" > submission.cpp
+cat num.hpp fmincg.hpp array2d.hpp linreg.hpp extract_subject_ranges.hpp ChildStuntedness5.hpp | grep -v "#include \"" > submission.cpp
 g++ -std=c++11 -c submission.cpp
 gvim submission.cpp &
